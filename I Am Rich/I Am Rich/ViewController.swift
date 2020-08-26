300f9436b4547cf2b6c3155be30087eb6e93dfa0
Aaron Lee <aaronlab.net@gmail.com>
2020-08-26 11:56:04 +0900

    UI

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Aaron Lee on 8/26/20.
+//  Copyright © 2020 Aaron Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
