6851cb27c33dc8713b2c6e15a4a91b2f594e295a
Aaron Lee <aaronlab.net@gmail.com>
2020-09-11 00:29:22 +0900

    egg timer complete

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -7,33 +7,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes: [String: Int] = ["Soft": 3, "Medium": 420, "Hard": 720]
-    var selectedTime = 0
     var timer = Timer()
+    var player: AVAudioPlayer?
+    
+    let eggTimes: [String: Int] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsPassed = 0
+    var totalTime = 60
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.progressBar.progress = 0
+    }
 
     @IBAction func eggsBtnTapped(_ sender: UIButton) {
-        timer.invalidate()
+        self.timer.invalidate()
         
         let hardness = sender.currentTitle!
+        self.totalTime = eggTimes[hardness]!
         
-        selectedTime = eggTimes[hardness]!
+        self.lblTitle.text = "How do you like your eggs?"
+        self.progressBar.progress = 0.0
+        self.secondsPassed = 0
         
-        timer = Timer.scheduledTimer(
+        self.timer = Timer.scheduledTimer(
             timeInterval: 1.0, target: self,
             selector: #selector(self.updateCounter), userInfo: nil, repeats: true)
+        
     }
     
     @objc func updateCounter() {
-        if (selectedTime > 0) {
-            print("\(selectedTime) seconds")
-            selectedTime -= 1
+        if (self.secondsPassed < totalTime) {
+            self.progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            self.secondsPassed += 1
         } else {
-            lblTitle.text = "DONE!"
+            self.lblTitle.text = "DONE!"
+            self.progressBar.progress = 1.0
+            
+            guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+            
+            do {
+                self.player = try AVAudioPlayer(contentsOf: url)
+                guard let player = self.player else { return }
+                player.numberOfLoops = 0
+                player.play()
+            } catch {
+                print("Error")
+            }
         }
     }
     
