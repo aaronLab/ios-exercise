30279d57030fe1073393c70d8fe80acb923e783c
Aaron Lee <aaronlab.net@gmail.com>
2020-09-10 21:52:23 +0900

    init egg timer

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    let eggTimes: [String: Int] = ["Soft": 3, "Medium": 420, "Hard": 720]
+    var selectedTime = 0
+    var timer = Timer()
 
-
+    @IBAction func eggsBtnTapped(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        
+        selectedTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(
+            timeInterval: 1.0, target: self,
+            selector: #selector(self.updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        if (selectedTime > 0) {
+            print("\(selectedTime) seconds")
+            selectedTime -= 1
+        } else {
+            lblTitle.text = "DONE!"
+        }
+    }
+    
 }
