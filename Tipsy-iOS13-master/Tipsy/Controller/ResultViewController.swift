0d899df430b15f2e1de9a69117a78bb9ab64b267
Aaron Lee <aaronlab.net@gmail.com>
2020-09-12 00:19:50 +0900

    setup: IBActions & IBOutlets

---
+++ Tipsy-iOS13-master/Tipsy/Controller/ResultViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Aaron Lee on 9/12/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
