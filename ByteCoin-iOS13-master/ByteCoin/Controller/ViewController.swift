e6304b239721f8d94ca62e7e5a0652ba7c3ab5b6
Aaron Lee <aaronlab.net@gmail.com>
2020-09-13 18:58:48 +0900

    complete: ByteCoin Project

--- ByteCoin-iOS13-master/ByteCoin/Controller/ViewController.swift
+++ ByteCoin-iOS13-master/ByteCoin/Controller/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.coinManager.delegate = self
+        self.currencyPicker.dataSource = self
+        self.currencyPicker.delegate = self
     }
-
-
+    
 }
 
+//MARK: - UIPickerViewDataSource, UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = self.coinManager.currencyArray[row]
+        self.currencyLabel.text = selectedCurrency
+        self.coinManager.getCoinPrice(for: selectedCurrency)
+    }
+    
+}
+
+//MARK: - CoinManagerDelegate
+
+extension ViewController: CoinManagerDelegate {
+    func didUpdateRate(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        // error handle
+        print(error)
+    }
+    
+    
+}
--- ByteCoin-iOS13-master/ByteCoin/Model/CoinManager.swift
+++ ByteCoin-iOS13-master/ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,53 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didUpdateRate(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
+    var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "YOUR_API_KEY"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
+    
+    func getCoinPrice(for currency: String) {
+        let urlStr = "\(self.baseURL)/\(currency)?apikey=\(self.apiKey)"
+        
+        if let url = URL(string: urlStr) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    // error handle
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let rate = self.parseJSON(safeData) {
+                        let price = String(format: "%.2f", rate)
+                        self.delegate?.didUpdateRate(price: price, currency: currency)
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            return decodedData.rate
+        } catch {
+            // error handle
+            self.delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
     
 }
