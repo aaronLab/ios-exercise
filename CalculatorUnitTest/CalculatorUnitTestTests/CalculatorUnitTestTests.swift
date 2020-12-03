7df71dc4c8689206937b3121062114f8fae2462f
Aaron Lee <aaronlab.net@gmail.com>
2020-12-03 21:19:47 +0900

    Refactoring Unit Test

--- CalculatorUnitTest/CalculatorUnitTestTests/CalculatorUnitTestTests.swift
+++ CalculatorUnitTest/CalculatorUnitTestTests/CalculatorUnitTestTests.swift
@@ -9,23 +9,31 @@ import XCTest
 @testable import CalculatorUnitTest
 
 class CalculatorUnitTestTests: XCTestCase {
+    
+    private var calculator: Calculator!
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.calculator = Calculator()
+    }
 
     func test_AddTwoNumbers() {
         
-        let calculator = Calculator()
-        let result = calculator.add(2, 3)
-        
+        let result = self.calculator.add(2, 3)
         XCTAssertEqual(result, 5)
         
     }
     
     func test_SubtractTwoNumbers() {
         
-        let calculator = Calculator()
-        let result = calculator.subtract(5, 2)
-        
+        let result = self.calculator.subtract(5, 2)
         XCTAssertEqual(result, 3)
         
+    }
+    
+    override func tearDown() {
+        super.tearDown()
     }
 
 }
