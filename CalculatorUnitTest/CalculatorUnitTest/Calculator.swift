85e9330f758661c5ec94d1ccf2474812b703f2c4
Aaron Lee <aaronlab.net@gmail.com>
2020-12-03 21:15:56 +0900

    Unit test

---
+++ CalculatorUnitTest/CalculatorUnitTest/Calculator.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator.swift
+//  CalculatorUnitTest
+//
+//  Created by Aaron Lee on 2020-12-03.
+//
+
+import Foundation
+
+class Calculator {
+    
+    func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+    
+    func subtract(_ a: Int, _ b: Int) -> Int {
+        return a - b
+    }
+    
+}
