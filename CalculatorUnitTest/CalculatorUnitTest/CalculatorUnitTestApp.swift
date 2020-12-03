29aa5c65f72764506bd949bd40b23265e1f7dbf6
Aaron Lee <aaronlab.net@gmail.com>
2020-12-03 21:04:46 +0900

    Init

---
+++ CalculatorUnitTest/CalculatorUnitTest/CalculatorUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorUnitTestApp.swift
+//  CalculatorUnitTest
+//
+//  Created by Aaron Lee on 2020-12-03.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorUnitTest/CalculatorUnitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorUnitTest
+//
+//  Created by Aaron Lee on 2020-12-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
